3fd576f042e6a0adde73ff58d8f4d495780da29f
Beastie <corypcole@gmail.com>
2016-10-16 16:05:09 -0400

    added gulp task copy cbp files from node_modules styled staff table with cbp theme - striped alt rows + hover styling

--- app/app.js
+++ app/app.js
@@ -4,6 +4,7 @@ require('angular');
 require('./controllers/dataEntry.controller');
 require('./controllers/dataDisplay.controller');
 require('./services/staff.service.js');
+//require('angular-ui-bootstrap');
 
 //setting the staffWizard Module - Only set this once then retrieve from this point after if I want to add stuff to it
 angular.module('staffWizard', ['dataDisplay']);

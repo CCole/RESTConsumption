5d7f8406d3c729cfcc285e1041f04210f6557b20
Beastie <corypcole@gmail.com>
2016-10-23 10:48:04 -0400

    Added angular ui for the popover on npiNumber input. Added logic to remove placeholder when npiNumber string length is 0. This happens when a user enters an incomplete npi number and clicks away or enters a complete number then deletes and clicks away.

--- app/app.js
+++ app/app.js
@@ -5,11 +5,12 @@ require('./controllers/dataEntry.controller');
 require('./controllers/dataDisplay.controller');
 require('./services/staff.service.js');
 require('angular-ui-mask');
-//require('angular-ui-bootstrap');
+require('angular-ui-bootstrap');
 
 //setting the staffWizard Module - Only set this once then retrieve from this point after if I want to add stuff to it
 angular.module('staffWizard', 
 ['dataDisplay',
 'dataEntry',
-'ui.mask'
+'ui.mask',
+'ui.bootstrap'
 ]);
--- app/controllers/dataEntry.controller.js
+++ app/controllers/dataEntry.controller.js
@@ -15,7 +15,12 @@ function enterData(staffData){
 
         vm.removeMask = function(){
             //if there are no values in the form input then remove mask on blur
-            if(typeof vm.npiNumber === "undefined"){
+            //not clear if angular-ui-mask implements a function that removes the placeholder if a user does not 
+            //finish entry and then clicks away. Definitly rethink this.
+          /* if(typeof vm.npiNumber !== "undefined") {
+               console.log(vm.npiNumber.length);
+           }*/
+            if(typeof vm.npiNumber === "undefined" || (vm.npiNumber.length == 0)){ 
                 vm.npiMask = "";
             }
         };

1b0b27e8e0cae8c9fb423db73350a726b0ea7c98
Beastie <corypcole@gmail.com>
2017-01-05 12:29:29 -0500

    added vm data to input value attribute to display data entered by users across form page navigation. Moved npi mask function to modal controller to register MDL components after each template render.

--- app/controllers/modal.controller.js
+++ app/controllers/modal.controller.js
@@ -7,6 +7,22 @@ module.exports =
 
 function getModal($uibModalInstance, $scope, $timeout) {
     var vm = this;
+    vm.placeMask = function () {
+        vm.npiMask = "9999999999";
+    };
+
+    vm.removeMask = function () {
+        //if there are no values in the form input then remove mask on blur
+        //not clear if angular-ui-mask implements a function that removes the placeholder if a user does not 
+        //finish entry and then clicks away. Definitly rethink this.
+        /* if(typeof vm.npiNumber !== "undefined") {
+             console.log(vm.npiNumber.length);
+         }*/
+        if (typeof vm.npiNumber === "undefined" || (vm.npiNumber.length == 0)) {
+            vm.npiMask = "";
+        }
+    };
+
     vm.cancelModal = function () {
         $uibModalInstance.close();
         console.log("modal closed");
